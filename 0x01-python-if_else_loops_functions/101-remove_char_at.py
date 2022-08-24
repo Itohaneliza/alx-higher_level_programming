14019b2b2d74cd03f9ef29a95f82a283643772b7
Itohaneliza <juliuseli2004@gmail.com>
2022-08-24 22:52:56 +0100

    my commit

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    s = ""
-    for i in range(len(str)):
-        if i != n:
-            s = s + str[i]
-    return (s)
+    part = str[:n]
+    parts = str[n+1:]
+    return part + parts
